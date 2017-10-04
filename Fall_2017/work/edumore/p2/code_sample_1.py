a9043b8e09b1f698c1e1f32ba5f6c2a242da8d10
edumore <31488812+edumore@users.noreply.github.com>
2017-10-04 16:17:37 -0400

    10.4.17

---
+++ Fall_2017/work/edumore/p2/code_sample_1.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import csv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+scope = ['https://spreadsheets.google.com/feeds']
+document_key = "13bsf7rP9ubfBSX7-hK3QayZIaUkTxrXaOZqGsfLjWX4"
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/ericadumore/Documents/2L Year/Semester 1/Coding in Law/SheetsBot-32718db5596a.json', scope)
+csvfile = "output.csv"
+gc = gspread.authorize(credentials)
+wks = gc.open_by_key(document_key)
+worksheet = wks.worksheet("Form Responses 1")
+list_of_lists = worksheet.get_all_values()
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerows(list_of_lists)
+
+import pandas as pd
+output = pd.read_csv(csvfile)
+output[:3]
+
