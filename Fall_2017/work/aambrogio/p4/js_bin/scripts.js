548c6ecf4c7aaf89a4e45f30ecfaadc6c0a116bb
aambrogio <31489534+aambrogio@users.noreply.github.com>
2017-12-18 15:57:24 -0500

    Commit Dec 18 2

--- Fall_2017/work/aambrogio/p4/js_bin/scripts.js
+++ Fall_2017/work/aambrogio/p4/js_bin/scripts.js
@@ -42,37 +42,37 @@ function sixmedinc(house,income){
 	if (house == 1) {
 		line = 34380;
 	} else if (house == 2) {
-		line = 16240;
+		line = 44958;
 	} else if (house == 3) {
-		line = 20420;
+		line = 55537;
 	} else if (house == 4) {
-		line = 24600;
+		line = 66115;
 	} else if (house == 5) {
-		line = 28780;
+		line = 76693;
 	} else if (house == 6) {
-		line = 32960;
+		line = 87272;
 	} else if (house == 7) {
-		line = 37140;
+		line = 89255;
 	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
-	} else if (house == 8) {
-		line = 41320;
+		line = 91239;
+	} else if (house == 9) {
+		line = 93222;
+	} else if (house == 10) {
+		line = 95206;
+	} else if (house == 11) {
+		line = 97189;
+	} else if (house == 12) {
+		line = 99173;
+	} else if (house == 13) {
+		line = 101156;
+	} else if (house == 14) {
+		line = 103139;
+	} else if (house == 15) {
+		line = 105123;
+	} else if (house == 16) {
+		line = 107106;
+	} else if (house == 17) {
+		line = 109090;
 	} else {
 		alert("Note: Cannot accept household of more that 17.");
 	}
